b233e15f884182d64567bd3071dedb084d892aca
Neven Pavelić <neven.pavelic@infinum.hr>
2018-07-19 20:26:21 +0200

    Add empty login screen

---
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  TVShows
+//
+//  Created by Neven Pavelić on 19/07/2018.
+//  Copyright © 2018 Neven Pavelić. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
